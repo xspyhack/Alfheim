0da84cb733ad94eb78e35a118e6e3a74a61f0974
xspyhack <xspyhack@gmail.com>
2020-02-14 00:12:05 +0800

    models

--- Alfheim/Models/Transaction.swift
+++ Alfheim/Models/Transaction.swift
@@ -11,8 +11,36 @@ import Foundation
 struct Transaction {
   var date: Date
   var amount: Double
+  var emoji: String
   var notes: String
-  var account: Bool?
-  var payee: String?
-  var number: Int
+  var currency: Currency = .cny
+  var payee: String? = nil
+  var number: Int = 0
+  var from: Account? = Accounts.income
+  var to: Account? = Accounts.expenses
+}
+
+enum Currency: Int {
+  case cny
+  case hkd
+  case jpy
+  case usd
+
+  var text: String {
+    switch self {
+    case .cny: return "CNY"
+    case .hkd: return "HKD"
+    case .jpy: return "JPY"
+    case .usd: return "USD"
+    }
+  }
+
+  var symbol: String {
+    switch self {
+    case .cny: return "¥"
+    case .hkd: return "$"
+    case .jpy: return "¥"
+    case .usd: return "$"
+    }
+  }
 }
