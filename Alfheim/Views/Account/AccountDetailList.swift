ce655a4fb95a5be2b87c4d10bf05db17cfb6edf7
xspyhack <xspyhack@gmail.com>
2020-03-04 22:02:07 +0800

    Update checkmark font weight

--- Alfheim/Views/Account/AccountDetailList.swift
+++ Alfheim/Views/Account/AccountDetailList.swift
@@ -26,6 +26,7 @@ struct AccountDetailList: View {
             Spacer()
             if tag == self.account.tag {
               Image(systemName: "checkmark")
+                .font(Font.body.bold())
                 .foregroundColor(.blue)
             }
           }
